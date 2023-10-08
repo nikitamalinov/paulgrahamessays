cd45c155cb738637ba832266026c524c0da2b0a6
Nikita Malinovsky <nikita@malinovsky.net>
2023-10-08 11:56:21 -0700

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,26 @@
 import "@/styles/globals.css";
 import MainLayout from "../components/Layouts/main";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Analytics } from "@vercel/analytics/react";
-
+import Script from "next/script";
 import theme from "@/theme/styles";
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <MainLayout>
         <Component {...pageProps} />
-        <Analytics />
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NDLJW640DH"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NDLJW640DH');`}
+        </Script>
       </MainLayout>
     </ChakraProvider>
   );
