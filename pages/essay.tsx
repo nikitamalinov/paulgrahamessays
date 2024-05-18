fc3b730a2ad186dfb78933f5f1f2a26d3badc00f
nikitamalinov <nikita@malinovsky.net>
2024-05-18 14:36:18 -0700

    .

--- pages/essay.tsx
+++ pages/essay.tsx
@@ -1,13 +1,18 @@
-import { useRouter } from 'next/router';
-import Head from 'next/head';
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 const EssayPage = () => {
   const router = useRouter();
   const { link } = router.query;
   return (
     <>
-      <Head><base target="_parent" /></Head>
-      <iframe src={link} style={{ width: '100%', height: '100vh', border: 'none' }} />
+      <Head>
+        <base target="_parent" />
+      </Head>
+      <iframe
+        src={typeof link === "string" ? link : ""}
+        style={{ width: "100%", height: "100vh", border: "none" }}
+      />
     </>
   );
 };
