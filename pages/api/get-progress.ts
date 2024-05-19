4c535dafb156f7ebf01bd49f43a56dc2979371cc
nikitamalinov <nikita@malinovsky.net>
2024-05-19 15:43:04 -0700

    Fixing types

--- pages/api/get-progress.ts
+++ pages/api/get-progress.ts
@@ -7,24 +7,19 @@ import { z, ZodError } from "zod";
 const schema = z.object({
   email: z.string(),
   idToken: z.string(),
-  lessonUrl: z.string(),
-  completed: z.boolean(),
 });
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { lessonUrl, completed, email, idToken } = JSON.parse(req.body);
+  const { email, idToken } = schema.parse(req.query);
+
   if (!isValidToken(email, idToken)) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  if (completed) {
-    await kv.sadd(email, lessonUrl);
-  } else {
-    await kv.srem(email, lessonUrl);
-  }
+  const completedLessons = await kv.smembers(email);
 
-  res.status(200).json({ message: "Progress saved" });
+  res.status(200).json({ completedLessons });
 }
