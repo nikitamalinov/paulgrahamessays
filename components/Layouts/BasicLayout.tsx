3acf59d616b1048aa49bda0a5a2039c3a95b9bba
nikitamalinov <nikita@malinovsky.net>
2024-05-19 15:36:00 -0700

    .

--- components/Layouts/BasicLayout.tsx
+++ components/Layouts/BasicLayout.tsx
@@ -1,15 +1,13 @@
-import Head from 'next/head'
-import Navbar from '@/components/Navbar'
-import React, { ReactNode, useEffect } from 'react'
-
-import { useSession } from 'next-auth/react'
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import React, { ReactNode } from "react";
 
 interface Props {
-  children?: ReactNode
-  title: string
-  overrideTitle?: boolean
-  isWhite?: boolean
-  overrideScreen?: string
+  children?: ReactNode;
+  title: string;
+  overrideTitle?: boolean;
+  isWhite?: boolean;
+  overrideScreen?: string;
 }
 
 export default function BasicLayout({
@@ -17,25 +15,23 @@ export default function BasicLayout({
   title,
   overrideTitle = false,
   isWhite = true,
-  overrideScreen = 'min-h-screen',
-  const { data: session } = useSession()
+  overrideScreen = "min-h-screen",
 }: Props) {
-  let message = ''
+  let message = "";
   if (!overrideTitle) {
-    message = title 
-        {session ? children : <p>Please sign in to access the content.</p>}
+    message = title;
   } else {
-    message = title
+    message = title;
   }
   return (
     <div>
       <Head>
         <title>{message}</title>
       </Head>
-      <div className={`${overrideScreen} ${isWhite ? 'bg-white' : 'bg-light'}`}>
+      <div className={`${overrideScreen} ${isWhite ? "bg-white" : "bg-light"}`}>
         <Navbar />
         {children}
       </div>
     </div>
-  )
+  );
 }
